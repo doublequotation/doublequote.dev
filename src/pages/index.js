cd44d2038bcd082557ab294663260e30a29113a6
ashtyn3 <ashtyn@gitmono.com>
2020-12-09 08:52:30 -0500

    fixed capitalization

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>Double quote</title>
+                <title>Double Quote</title>
                 <meta
                     name="viewport"
                     content="initial-scale=1.0, width=device-width"
